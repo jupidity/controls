42431c9dce4a202a15e4ab25ba1caaecd0d27179
Sean Cassero <sacassero@gmail.com>
2017-08-27 12:14:42 -0400

    initial shell for hover controller node

---
+++ quad_controller/src/hover_controller_node.cpp
@@ -0,0 +1,20 @@
+/*
+
+ROS node for controlling quadcopter hover in Unity sim
+
+Author: Sean Cassero
+
+
+*/
+
+#include "pid_controller.h"
+
+
+
+int main() {
+
+
+PIDController pid;
+
+  return 0;
+}
